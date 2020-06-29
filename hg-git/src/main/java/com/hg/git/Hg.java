3af9450a56d254b3386fcc6240c44ee04479c72f
songbo <884790950@qq.com>
2020-06-29 11:28:55 +0800

    修改了Hg

--- hg-git/src/main/java/com/hg/git/Hg.java
+++ hg-git/src/main/java/com/hg/git/Hg.java
@@ -4,5 +4,9 @@ public class Hg {
 
     public static void main(String[] args) {
         System.out.println("hg git......");
+
+        System.out.println("dinfufuewu");
+        System.out.println("dinfufuewu");
+        System.out.println("dinfufuewu");
     }
 }
