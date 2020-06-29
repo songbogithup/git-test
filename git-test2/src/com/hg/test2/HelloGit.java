4da7eccdbf035b842886d4f7856a154674c2e9b6
songbo <884790950@qq.com>
2020-06-29 11:07:06 +0800

    修改了HelloGit

--- git-test2/src/com/hg/test2/HelloGit.java
+++ git-test2/src/com/hg/test2/HelloGit.java
@@ -5,4 +5,9 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git...");
     }
+
+    public void test(){
+        int i = 0;
+        i = i + 10;
+    }
 }
